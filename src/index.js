d54d10e7dc28ce0bb1fca6b5e096b673277feee2
Alan Erriu <erriu0273@gmail.com>
2023-07-02 12:06:02 -0300

    deploy-settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import app from "./app";
 import connect from "./database";
 
-app.listen(3000);
 connect();
-console.log("server on port ", 3000);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
