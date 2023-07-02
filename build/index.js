7387006e0421a7d0774e72c75474414260343598
Alan Erriu <erriu0273@gmail.com>
2023-07-02 12:55:15 -0300

    build

--- build/index.js
+++ build/index.js
@@ -3,6 +3,8 @@
 var _app = _interopRequireDefault(require("./app"));
 var _database = _interopRequireDefault(require("./database"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_app["default"].listen(3000);
 (0, _database["default"])();
-console.log("server on port ", 3000);
+var PORT = 3000;
+_app["default"].listen(PORT, function () {
+  console.log("Server is running on port", PORT);
+});
