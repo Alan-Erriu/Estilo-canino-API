89b7c51ac342c5bbb41ee6befe5d98e6570a0e94
Alan Erriu <erriu0273@gmail.com>
2023-07-02 13:02:51 -0300

    rename-file dataBase for database

--- build/index.js
+++ build/index.js
@@ -2,7 +2,9 @@
 
 var _app = _interopRequireDefault(require("./app"));
 var _database = _interopRequireDefault(require("./database"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 (0, _database["default"])();
 var PORT = 3000;
 _app["default"].listen(PORT, function () {
