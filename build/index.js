ff3a023551e11dab41dd76b8e9ae1a8abc705e82
Alan Erriu <erriu0273@gmail.com>
2023-07-02 13:51:50 -0300

    fixes in deploy

--- build/index.js
+++ build/index.js
@@ -1,9 +1,11 @@
 "use strict";
 
 var _app = _interopRequireDefault(require("./app"));
-var _database = _interopRequireDefault(require("./database"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-(0, _database["default"])();
+var _dataBase = _interopRequireDefault(require("./database"));
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+(0, _dataBase["default"])();
 var PORT = 3000;
 _app["default"].listen(PORT, function () {
   console.log("Server is running on port", PORT);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from "./app";
-import connect from "./database";
+import connect from "./dataBase";
 
 connect();
 
